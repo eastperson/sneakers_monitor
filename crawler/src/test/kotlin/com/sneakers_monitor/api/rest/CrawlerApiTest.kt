7a69ae989c6ce6c30fcba4d7636af144f4dd9fc1
EP <kjuioqq8@gmail.com>
2022-04-03 14:12:09 +0900

    test: API Test 추가

--- crawler/src/test/kotlin/com/sneakers_monitor/api/rest/CrawlerApiTest.kt
+++ crawler/src/test/kotlin/com/sneakers_monitor/api/rest/CrawlerApiTest.kt
@@ -1,12 +1,39 @@
 package com.sneakers_monitor.api.rest
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.sneakers_monitor.crawler.api.ApiResponse
+import com.sneakers_monitor.crawler.repository.ProductRepository
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 
-import org.junit.jupiter.api.Assertions.*
+internal class CrawlerApiTest: RestControllerTest() {
 
-internal class CrawlerApiTest {
+    @Autowired
+    lateinit var productRepository: ProductRepository
+
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
 
     @Test
-    fun nikeCrawler() {
+    fun `나이키 데이터를 크롤링하고 저장한다`() {
+        val result = mockMvc.get("/api/crawl/nike") {
+
+            }.andDo {
+                print()
+            }.andExpect {
+                status { isOk() }
+                jsonPath("data.size").exists()
+                jsonPath("data").exists()
+            }.andReturn()
+        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true)
+        val response = objectMapper.readValue(result.response.contentAsString, ApiResponse::class.java)
+        val body:LinkedHashMap<String, Object> = response.body as LinkedHashMap<String, Object>
+        Assertions.assertThat(response).isNotNull
+        Assertions.assertThat(body).isNotNull
+        Assertions.assertThat(body["size"] as Int).isEqualTo(productRepository.findAll().size)
     }
 }
--- crawler/src/test/kotlin/com/sneakers_monitor/api/rest/RestControllerTest.kt
+++ crawler/src/test/kotlin/com/sneakers_monitor/api/rest/RestControllerTest.kt
@@ -1,4 +1,28 @@
 package com.sneakers_monitor.api.rest
 
-class RestControllerTest {
+import com.sneakers_monitor.crawler.CrawlerApplication
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestConstructor
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.web.filter.CharacterEncodingFilter
+
+@SpringBootTest(classes = [CrawlerApplication::class])
+@ActiveProfiles("test")
+open class RestControllerTest {
+
+    lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    fun setup(webApplicationContext: WebApplicationContext) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .addFilters<DefaultMockMvcBuilder>(CharacterEncodingFilter("UTF-8", true))
+            .alwaysDo<DefaultMockMvcBuilder> { MockMvcResultHandlers.print()}
+            .build()
+    }
 }
