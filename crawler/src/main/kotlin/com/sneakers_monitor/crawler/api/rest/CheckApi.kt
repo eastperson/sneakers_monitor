db72a95bd23277efc16a6b127e092f93557faed9
EP <kjuioqq8@gmail.com>
2022-04-05 23:15:16 +0900

    feat: Check Controller 추가

---
+++ crawler/src/main/kotlin/com/sneakers_monitor/crawler/api/rest/CheckApi.kt
@@ -0,0 +1,20 @@
+package com.sneakers_monitor.crawler.api.rest
+
+import com.sneakers_monitor.crawler.api.ApiResponse
+import com.sneakers_monitor.crawler.service.nike.NikeCrawler
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.transaction.Transactional
+
+@Transactional
+@RequestMapping("/api/check")
+@RestController
+class CheckApi {
+
+    @GetMapping("")
+    fun nikeCrawler():ResponseEntity<*> {
+        return ResponseEntity.ok(ApiResponse.success(body = "OK"))
+    }
+}
