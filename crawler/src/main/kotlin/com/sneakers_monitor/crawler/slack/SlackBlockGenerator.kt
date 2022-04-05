dabf59de6eec711d72169919828be8da9b121615
EP <kjuioqq8@gmail.com>
2022-04-05 09:55:05 +0900

    feat: 슬랙 block kit 추가

--- crawler/src/main/kotlin/com/sneakers_monitor/crawler/slack/SlackBlockGenerator.kt
+++ crawler/src/main/kotlin/com/sneakers_monitor/crawler/slack/SlackBlockGenerator.kt
@@ -1,4 +1,75 @@
 package com.sneakers_monitor.crawler.slack
 
+import com.slack.api.model.block.Blocks.*
+import com.slack.api.model.block.HeaderBlock.HeaderBlockBuilder
+import com.slack.api.model.block.LayoutBlock
+import com.slack.api.model.block.SectionBlock.SectionBlockBuilder
+import com.slack.api.model.block.composition.BlockCompositions.markdownText
+import com.slack.api.model.block.composition.BlockCompositions.plainText
+import com.slack.api.model.block.element.BlockElements.*
+import com.slack.api.model.block.element.ButtonElement.ButtonElementBuilder
+import com.slack.api.model.block.element.ImageElement.ImageElementBuilder
+import com.slack.api.webhook.Payload
+import com.slack.api.webhook.Payload.PayloadBuilder
+import com.slack.api.webhook.WebhookPayloads
+import com.sneakers_monitor.crawler.domain.Brand
+import com.sneakers_monitor.crawler.service.nike.ProductDto
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+@Component
 class SlackBlockGenerator {
+
+    fun generateTitle(brand:Brand):List<LayoutBlock> {
+        val layoutBlocks:MutableList<LayoutBlock> = arrayListOf()
+        val now = LocalDateTime.now()
+        layoutBlocks.add(header { plainText:HeaderBlockBuilder -> plainText.text(plainText("${now.year}년 ${now.month.value}월 ${now.dayOfMonth}일 $brand 발매 정보"))})
+        layoutBlocks.add(divider())
+        return layoutBlocks
+    }
+
+    fun generateBlock(product: ProductDto):List<LayoutBlock> {
+        val layoutBlocks:MutableList<LayoutBlock> = arrayListOf()
+        layoutBlocks.add(section { section: SectionBlockBuilder ->
+            section.text(
+                markdownText(
+                    """이름 : ${product.title}
+                        |브랜드 : ${product.brand}
+                        |출시일 : ${product.date}
+                        |상품 색상 : ${product.color}
+                        |상품 가격 : ${product.price}
+                    """.trimMargin()
+                )
+            ).accessory(image { image: ImageElementBuilder ->
+                image.imageUrl(
+                    product.image
+                ).altText("상품 사진")
+            })
+        })
+        layoutBlocks.add(section { section: SectionBlockBuilder ->
+            section.text(markdownText(" ")).accessory(
+                button { button: ButtonElementBuilder ->
+                    button.text(plainText("상품 보러가기")).url(product.url)
+                }
+            )
+        })
+        layoutBlocks.add(divider())
+        return layoutBlocks
+    }
+
+    fun generateErrorMessage(product: ProductDto):Payload {
+        return Payload.builder()
+            .text("슬랙에 메시지를 출력하지 못했습니다.")
+            .blocks(asBlocks(section { section: SectionBlockBuilder ->
+                section.text(
+                    markdownText(
+                        "상품 아이디 : ${product.id}등록 에러 발생"))
+            })).build()
+    }
+
+    fun generateMessage(layoutBlocks: List<LayoutBlock>):Payload {
+        return Payload.builder()
+            .text("슬랙에 메시지를 출력하지 못했습니다.")
+            .blocks(layoutBlocks).build()
+    }
 }
--- crawler/src/main/kotlin/com/sneakers_monitor/crawler/slack/SlackSender.kt
+++ crawler/src/main/kotlin/com/sneakers_monitor/crawler/slack/SlackSender.kt
@@ -1,4 +1,37 @@
 package com.sneakers_monitor.crawler.slack
 
-class SlackSender {
+import com.slack.api.Slack
+import com.slack.api.model.block.Blocks.asBlocks
+import com.slack.api.model.block.Blocks.section
+import com.slack.api.model.block.LayoutBlock
+import com.slack.api.model.block.SectionBlock.SectionBlockBuilder
+import com.slack.api.model.block.composition.BlockCompositions.markdownText
+import com.slack.api.webhook.Payload.PayloadBuilder
+import com.slack.api.webhook.WebhookPayloads.payload
+import com.sneakers_monitor.crawler.domain.Brand
+import com.sneakers_monitor.crawler.service.nike.ProductDto
+import org.springframework.stereotype.Component
+import java.io.IOException
+
+
+@Component
+class SlackSender(val slackBlockGenerator: SlackBlockGenerator){
+
+    fun send(products:List<ProductDto>, brand: Brand) {
+        try {
+            Slack.getInstance().send(SlackChannel.RAFFLE.url, slackBlockGenerator.generateMessage(slackBlockGenerator.generateTitle(brand)))
+        } catch (e:Exception) {
+            println(e.message)
+        }
+        for (product in products) {
+            val layoutBlocks = slackBlockGenerator.generateBlock(product)
+            try {
+                Slack.getInstance().send(SlackChannel.RAFFLE.url, slackBlockGenerator.generateMessage(layoutBlocks))
+            } catch (ie: IOException) {
+                println(ie.message)
+            } catch (e:Exception) {
+                Slack.getInstance().send(SlackChannel.RAFFLE.url, slackBlockGenerator.generateErrorMessage(product))
+            }
+        }
+    }
 }
