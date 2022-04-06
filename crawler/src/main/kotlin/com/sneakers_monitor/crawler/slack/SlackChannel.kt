4be8dbb62d689bc4b00f9e5615691dad83c6aa77
EP <kjuioqq8@gmail.com>
2022-04-06 16:53:53 +0900

    refactor: web hook 주소 변경

--- crawler/src/main/kotlin/com/sneakers_monitor/crawler/slack/SlackChannel.kt
+++ crawler/src/main/kotlin/com/sneakers_monitor/crawler/slack/SlackChannel.kt
@@ -1,5 +1,5 @@
 package com.sneakers_monitor.crawler.slack
 
 enum class SlackChannel(name: String, val descr: String, val url: String) {
-    RAFFLE("#raffle", "금일 한정판 스니커즈 정보", "https://hooks.slack.com/services/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");
+    RAFFLE("#raffle", "금일 한정판 스니커즈 정보", "https://hooks.slack.com/services/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");
 }
